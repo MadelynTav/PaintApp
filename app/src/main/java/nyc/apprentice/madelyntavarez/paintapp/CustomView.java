e034a73b78f45c86a938297bd4335d5d6743f711
madelyntav <madelyntav@gmail.com>
2016-01-05 17:58:34 -0500

    added functionality to color, brush and erase buttons

--- app/src/main/java/nyc/apprentice/madelyntavarez/paintapp/CustomView.java
+++ app/src/main/java/nyc/apprentice/madelyntavarez/paintapp/CustomView.java
@@ -6,10 +6,17 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.PaintDrawable;
 import android.graphics.drawable.shapes.Shape;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
+
+import butterknife.Bind;
+import butterknife.BindDimen;
 
 /**
  * Created by madelyntavarez on 1/4/16.
@@ -20,10 +27,14 @@ import android.view.View;
 public class CustomView extends View {
 
     private Path drawPath;
-    private Paint canvasPaint, drawPaint;
+    Paint canvasPaint, drawPaint;
     private int paintColor= 0xFF660000;
     private Canvas drawCanvas;
-    private Bitmap bitmap;
+    float brushSize;
+    float lastBrushSize;
+    private Bitmap canvasBitmap;
+    private boolean erase;
+    private int lastColor;
 
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -35,13 +46,97 @@ public class CustomView extends View {
         drawPaint = new Paint();
         drawPaint.setColor(paintColor);
 
+
         drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20f);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
         drawPaint.setStrokeJoin(Paint.Join.ROUND);
         drawPaint.setStrokeCap(Paint.Cap.ROUND);
 
         canvasPaint = new Paint(Paint.DITHER_FLAG);
+
+
+        brushSize= getResources().getInteger(R.integer.medium_size);
+        lastBrushSize = brushSize;
+        drawPaint.setStrokeWidth(brushSize);
     }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
 
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+
+    }
+
+    public void setBrushSize(float newSize){
+        invalidate();
+        if (!isErase()){
+            drawPaint.setColor(lastColor);
+        }
+        float pixelAmount= TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, getResources().getDisplayMetrics());
+        brushSize=pixelAmount;
+        drawPaint.setStrokeWidth(brushSize);
+    }
+
+    public void setLastBrushSize(float lastBrushSize){
+        this.lastBrushSize=lastBrushSize;
+        setBrushSize(lastBrushSize);
+    }
+
+    public void setPaintColor(int paintColor) {
+        invalidate();
+        brushSize=lastBrushSize;
+        drawPaint.setStrokeWidth(brushSize);
+        this.paintColor = paintColor;
+        lastColor=paintColor;
+        drawPaint.setColor(paintColor);
+
+    }
+
+    public boolean isErase() {
+        return erase;
+    }
+
+    public void setErase(boolean erase) {
+        this.erase = erase;
+
+        if(isErase()) {
+            drawPaint.setColor(Color.WHITE);
+        }
+
+        else drawPaint.setXfermode(null);
+
+    }
 }
--- app/src/main/java/nyc/apprentice/madelyntavarez/paintapp/MainActivity.java
+++ app/src/main/java/nyc/apprentice/madelyntavarez/paintapp/MainActivity.java
@@ -1,68 +1,112 @@
 package nyc.apprentice.madelyntavarez.paintapp;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import butterknife.Bind;
+import butterknife.BindDimen;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ImageButton currBrushSize;
+    ImageButton currColor;
+    ImageButton erase;
+    float smallBrush;
+    float mediumBrush;
+    float largeBrush;
+    private CustomView customView;
+    Dialog colorDialog;
+    Context context=this;
+    Dialog brushDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
+        currBrushSize= (ImageButton) findViewById(R.id.brush_button);
+        currColor = (ImageButton) findViewById(R.id.color_button);
+        customView = (CustomView) findViewById(R.id.drawing_view);
+        erase = (ImageButton) findViewById(R.id.erase);
+        smallBrush = getResources().getInteger(R.integer.small_size);
+        mediumBrush = getResources().getInteger(R.integer.medium_size);
+        largeBrush = getResources().getInteger(R.integer.large_size);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        currBrushSize.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                customView.setErase(false);
+                brushDialog = new Dialog(context);
+                brushDialog.setContentView(R.layout.brush_size_picker);
+                brushDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                brushDialog.show();
+            }
+        });
+
+        currColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customView.setErase(false);
+                colorDialog = new Dialog(context);
+                colorDialog.setContentView(R.layout.color_picker);
+                colorDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                colorDialog.show();
+            }
+        });
+
+        erase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customView.setErase(true);
+                brushDialog = new Dialog(context);
+                brushDialog.setContentView(R.layout.brush_size_picker);
+                brushDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+                brushDialog.show();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void changeBrushSize(View v){
+        float tag= Integer.parseInt(String.valueOf(v.getTag()));
+        customView.setBrushSize(tag);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (!customView.isErase()) {
+            customView.setLastBrushSize(tag);
+            Log.i("size","UP");
         }
 
-        return super.onOptionsItemSelected(item);
+        brushDialog.dismiss();
     }
 
-    @Override
-    public void onClick(View v) {
-         if (v.getId()==R.id.color){
-             final Dialog colorDialog= new Dialog(this);
-             colorDialog.setTitle("Choose Color");
-             colorDialog.setContentView(R.layout.color_picker);
+    public void changeBrushColor(View v){
+        String color=v.getTag().toString();
+        int color1= Color.parseColor(color);
+        customView.setPaintColor(color1);
+        colorDialog.dismiss();
 
-         } else if (v.getId()==R.id.brush){
-             final Dialog brushDialog= new Dialog(this);
-             brushDialog.setTitle("Choose Brush Size");
-             brushDialog.setContentView(R.layout.brush_size_picker);
-         }
+
     }
 }
